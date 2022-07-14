e248a375e0da48698dafa3ec5e89f9a0c1f4c97e
German Ivanov <germanivanov0719@gmail.com>
2022-07-14 17:02:04 +0300

    New algorithm to calculate marks to goals specified appears to work fine now
    - add getMark() for convenient rounding to whole marks
    - make calcTillGoal() universal

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function newMean() {
   let m = parseFloat(document.getElementById("m-input").value);
   let w = parseFloat(document.getElementById("w-input").value);
 
-  res = (s * M1 + m * w) / (s + w);
+  let res = (s * M1 + m * w) / (s + w);
   if (!res) {
     alert("Недостаточно данных");
     return 0;
@@ -26,37 +26,47 @@ function newMean() {
 }
 
 function customRound(n) {
-  a = localStorage["perform-calculations-round"];
+  let a = localStorage["perform-calculations-round"];
   if (a && a != "false") {
     return Math.round(n * 100) / 100;
   }
   return n;
 }
 
-function calcTillGoal(mean, n, goal, mark = goal) {
-  console.log(mark);
-  let currentMean = mean;
-  let currentWeight = n;
-  r = 0;
-  maxGrade = parseFloat(goal + "." + localStorage["fraction_up"]);
-  minGrade = parseFloat(goal - 1 + "." + localStorage["fraction_up"]);
-  console.log(maxGrade, minGrade);
-  if (maxGrade >= mean || mean >= minGrade) {
-    return 0;
+function getMark(n) {
+  // 4.000007299376601 -> 5
+  // 4.000007299270041 -> 5
+  // 4.000007299163483 -> 5
+  // 4.0000072990569295 -> 5
+  // 4.000007298950378 -> 5
+  // 4.000007298843831 -> 5
+  let goal = parseInt(n);
+  let maxGrade = parseFloat(`${goal}.${localStorage["fraction_up"]}`);
+  let minGrade = parseFloat(`${goal - 1}.${localStorage["fraction_up"]}`);
+  if (minGrade <= n && n < maxGrade) {
+    console.log(n + " -> " + parseInt(goal));
+    return parseInt(goal);
+  } else if (n < minGrade) {
+    console.log(n + " -> " + parseInt(goal - 1));
+    return parseInt(goal - 1);
   }
-  if (mean <= minGrade) {
-    while (customRound(currentMean) < minGrade) {
-      r += 1;
-      currentMean = (currentMean * currentWeight + mark) / (currentWeight + 1);
-      currentWeight += 1;
-    }
+  console.log(n + " -> " + parseInt(goal + 1));
+  return parseInt(goal + 1);
+}
+
+function calcTillGoal(mean, weight, goal, mark = goal) {
+  console.log(mark, "Now: ", mean, " in q. ", weight);
+  let r = 0;
+  if (getMark(mean) == goal) {
+    console.log("For " + goal + ": already done");
     return r;
   }
-  while (customRound(currentMean) > maxGrade) {
-    console.log(currentMean);
-    r -= 1;
-    currentMean = (currentMean * currentWeight + mark) / (currentWeight + 1);
-    currentWeight += 1;
+  let negativeR = getMark(mean) > goal;
+  while (getMark(mean) != goal) {
+    // console.log(mean);
+    r += negativeR ? -1 : 1;
+    mean = (mean * weight + mark) / (weight + 1);
+    weight += 1;
   }
   return r;
 }
@@ -75,110 +85,17 @@ function tillGoals(mean, n) {
   if (mean == parseFloat(document.getElementById("M1-input").value)) {
     document.getElementById("M2-input").textContent = "Не изменялся";
   }
-  r = [];
+  let r = [];
   for (let i = 0; i < 4; i++) {
-    r += calcTillGoal(mean, n, 5 - i);
+    // console.log(calcTillGoal(mean, n, 5 - i));
+    r.push(calcTillGoal(mean, n, 5 - i));
   }
-  //   r = [calcTillGoal(mean, n, 5), 0, 0, 0];
-
-  //   // 5
-  //   console.log("5:");
-  //   c_m = mean;
-  //   c_n = n;
-  //   rq = 0;
-  //   if (mean >= parseFloat(4 + "." + f_rq)) {
-  //     r[0] = 0;
-  //   } else {
-  //     while (customRound(c_m) < parseFloat(4 + "." + f_rq)) {
-  //       rq += 1;
-  //       c_m = (c_m * c_n + 5) / (c_n + 1);
-  //       c_n += 1;
-  //       console.log(c_m);
-  //     }
-  //     r[0] = rq;
-  //   }
-
-  //   // 4
-  //   console.log("4:");
-  //   c_m = mean;
-  //   c_n = n;
-  //   rq = 0;
-  //   if (mean < parseFloat(3 + "." + f_rq)) {
-  //     while (customRound(c_m) < parseFloat(3 + "." + f_rq)) {
-  //       rq += 1;
-  //       c_m = (c_m * c_n + 4) / (c_n + 1);
-  //       c_n += 1;
-  //       console.log(c_m);
-  //     }
-  //   } else if (customRound(c_m) >= parseFloat(4 + "." + f_rq)) {
-  //     while (customRound(c_m) >= parseFloat(4 + "." + f_rq)) {
-  //       rq -= 1;
-  //       c_m = (c_m * c_n + 4) / (c_n + 1);
-  //       c_n += 1;
-  //       console.log(c_m);
-  //     }
-  //   }
-  //   r[1] = rq;
-
-  //   // 3
-  //   console.log("3:");
-  //   c_m = mean;
-  //   c_n = n;
-  //   rq = 0;
-  //   if (mean < parseFloat(2 + "." + f_rq)) {
-  //     while (customRound(c_m) < parseFloat(2 + "." + f_rq)) {
-  //       rq += 1;
-  //       c_m = (c_m * c_n + 3) / (c_n + 1);
-  //       c_n += 1;
-  //       console.log(c_m);
-  //     }
-  //   } else if (c_m >= parseFloat(3 + "." + f_rq)) {
-  //     while (c_m >= parseFloat(3 + "." + f_rq)) {
-  //       rq -= 1;
-  //       c_m = (c_m * c_n + 3) / (c_n + 1);
-  //       c_n += 1;
-  //       console.log(c_m);
-  //     }
-  //   }
-  //   r[2] = rq;
-
-  //   // 2
-  //   console.log("2:");
-  //   c_m = mean;
-  //   c_n = n;
-  //   rq = 0;
-  //   if (customRound(c_m) >= parseFloat(2 + "." + f_rq)) {
-  //     while (customRound(c_m) >= parseFloat(2 + "." + f_rq)) {
-  //       rq -= 1;
-  //       c_m = (c_m * c_n + 2) / (c_n + 1);
-  //       c_n += 1;
-  //       console.log(c_m);
-  //     }
-  //   }
-  //   r[3] = rq;
-
-  //   if (r[0] == 0) {
-  //     r[0] = "Готово";
-  //   }
-  //   if (r[1] == 0) {
-  //     r[1] = "Готово";
-  //   }
-  //   if (r[2] == 0) {
-  //     r[2] = "Готово";
-  //   }
-  //   if (r[3] == 0) {
-  //     r[3] = "Готово";
-  //   }
+  console.log(r);
   for (let i = 5; i > 1; i--) {
     if (r[5 - i] == 0) {
       r[5 - i] = "Готово";
     }
+
     document.getElementById("res-" + i).textContent = r[5 - i];
   }
-
-  //   document.getElementById("res-5").textContent = r[0];
-  //   document.getElementById("res-4").textContent = r[1];
-  //   document.getElementById("res-3").textContent = r[2];
-  //   document.getElementById("res-2").textContent = r[3];
-  // console.log(r)
 }
