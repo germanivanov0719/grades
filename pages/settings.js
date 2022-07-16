9d084a10008c9cdcd51c6fdece0a5e15eb29e903
German Ivanov <germanivanov0719@gmail.com>
2022-07-17 01:50:40 +0300

    Tried fixing settings (chackbox still broken on Safari), some layout improvements

--- pages/settings.js
+++ pages/settings.js
@@ -1,24 +1,33 @@
-load()
-function save(){
-    localStorage['fraction_up'] = document.getElementById("fraction-input").value;
-    localStorage['perform-calculations-round'] = document.getElementById("perform-calculations-round").checked;
-    console.log(localStorage)
-    if (customRound(0 + '.' + localStorage['fraction_up']) > 0 && localStorage['fraction_up'].length <= 3) {
-        alert("Сохранено")
-    }
-    else {
-        alert("Ошибка. Либо число не положительное (возможно, отключение округления поможет), либо слишком длинное. В целях воизбежания бесконечных/очень длительных вычислений, настройки будут сброшены.")
-        reset()
-    } 
+load();
+
+function save() {
+  localStorage["fraction_up"] = document.getElementById("fraction-input").value;
+  localStorage["perform-calculations-round"] = document.getElementById(
+    "perform-calculations-round"
+  ).checked;
+  console.log(localStorage);
+  if (
+    customRound(0 + "." + localStorage["fraction_up"]) > 0 &&
+    localStorage["fraction_up"].length <= 3
+  ) {
+    alert("Сохранено");
+  } else {
+    alert(
+      "Ошибка, число слишком длинное или отрицательное. Настройки будут сброшены."
+    );
+    reset();
+  }
 }
 
-function reset(){
-    localStorage['perform-calculations-round'] = 'True';
-    localStorage['fraction_up'] = '51';
-    window.location.reload()
+function reset() {
+  localStorage["perform-calculations-round"] = true;
+  localStorage["fraction_up"] = "51";
+  window.location.reload();
 }
 
-function load(){
-    document.getElementById("fraction-input").value = localStorage['fraction_up']
-    document.getElementById("perform-calculations-round").checked = (localStorage['perform-calculations-round'] == 'true')
+function load() {
+  document.getElementById("fraction-input").value = localStorage["fraction_up"];
+  document.getElementById("perform-calculations-round").checked = Boolean(
+    localStorage["perform-calculations-round"]
+  );
 }
