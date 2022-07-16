edd2ab66ce9f4f4a26a683c0256e3a5477f9d4e4
German Ivanov <germanivanov0719@gmail.com>
2022-07-16 18:15:36 +0300

    Fix absolute path to sw.js, which prevented PWA from installing on release

--- register.js
+++ register.js
@@ -13,7 +13,7 @@ window.addEventListener("load", () => {
   // Is service worker available?
   if ("serviceWorker" in navigator) {
     navigator.serviceWorker
-      .register("/sw.js")
+      .register("sw.js")
       .then(() => {
         console.log("Service worker registered!");
       })
