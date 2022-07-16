6cf3250cb9c29783b230647b7242487d1e84208e
German Ivanov <germanivanov0719@gmail.com>
2022-07-16 19:59:41 +0300

    Return to previous service worker

--- sw.js
+++ sw.js
@@ -30,12 +30,16 @@ const putInCache = async (request, response) => {
   await cache.put(request, response);
 };
 
-self.addEventListener("fetch", (event) => {
-  const responseFromCache = await caches.match(event.request);
+const cacheFirst = async (request) => {
+  const responseFromCache = await caches.match(request);
   if (responseFromCache) {
     return responseFromCache;
   }
   const responseFromNetwork = await fetch(request);
   putInCache(request, responseFromNetwork.clone());
   return responseFromNetwork;
+};
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(cacheFirst(event.request));
 });
