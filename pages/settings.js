a2055e78983bbbd2f573f374c87b83ad4d099711
German Ivanov <germanivanov0719@gmail.com>
2022-07-17 02:06:33 +0300

    Fix checkbox always enabled (still non-toggleable on Safari)

--- pages/settings.js
+++ pages/settings.js
@@ -20,14 +20,21 @@ function save() {
 }
 
 function reset() {
-  localStorage["perform-calculations-round"] = true;
+  localStorage["perform-calculations-round"] = "true";
   localStorage["fraction_up"] = "51";
   window.location.reload();
 }
 
 function load() {
   document.getElementById("fraction-input").value = localStorage["fraction_up"];
-  document.getElementById("perform-calculations-round").checked = Boolean(
-    localStorage["perform-calculations-round"]
-  );
+  if (localStorage["perform-calculations-round"] == "true") {
+    document
+      .getElementById("perform-calculations-round")
+      .setAttribute("checked", "");
+    document.getElementById("perform-calculations-round").checked = true;
+  } else {
+    document
+      .getElementById("perform-calculations-round")
+      .removeAttribute("checked");
+  }
 }
